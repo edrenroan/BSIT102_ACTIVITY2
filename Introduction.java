6e76407428b4514bb4d069fb16a881cf8078a933
edrenroan <boygreencross@gmail.com>
2019-08-13 09:40:01 +0800

    first commit

---
+++ Introduction.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Introduction {
+    
+    public static void main(String[] args) {
+    
+    Scanner objScanner1= new Scanner(System.in);
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Input Name:");
+    String x = scanner.nextLine();
+    
+    System.out.println("Input Course:");
+    String y = scanner.nextLine();
+    
+    System.out.println("Input Section:");
+    String z = scanner.nextLine();
+    
+    
+    System.out.println("Input Adress:");
+    String w = scanner.nextLine();
+    
+    System.out.println("Input Contact Number: ");
+    String u = scanner.nextLine();
+    
+    System.out.println("My Name is " + x);
+    System.out.println("My Course is " + y);
+    System.out.println("I live in " + w);
+    System.out.println("My contact number is " + u);
+    
+    }
+  
+    }
